82ca0699ec1640e6eeddea638aca141b9f54b2ce
ind-spirit <woulart@gmail.com>
2023-12-25 20:49:17 +0300

    mobile

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 window.onload = () => {
+  let about_article = document.querySelectorAll(".about_article")[1];
+  let contacts_article = document.querySelectorAll(".contacts_article")[1];
+    const ifMobile = window.matchMedia(
+        "screen and (max-aspect-ratio: 8/11)"
+    ).matches;
+    if (ifMobile) {
+        document.body.style.height = `${window.innerHeight}px`;
+        document
+            .querySelectorAll(".mobile_article-wrapper")[0]
+            .classList.remove("hidden");
+        about_article = document.querySelectorAll(".about_article")[0];
+        contacts_article =
+            document.querySelectorAll(".contacts_article")[0];
+    }
     // const ifMobile = window.matchMedia('screen and (max-aspect-ratio: 8/11)').matches;
     // if (ifMobile) {
     //     document.body.style.height = `${window.innerHeight}px`;
@@ -32,11 +46,9 @@ window.onload = () => {
     const counter2 = document.getElementsByClassName("counter")[1];
     const images = document.querySelectorAll("div.images_wrapper > img");
     let images_counter = images.length;
-    const about_btn = document.querySelector('#about-btn');
-    const contact_btn = document.querySelector('#contact-btn');
-    const hidden_wrapper = document.querySelectorAll('.hidden-wrapper')[0];
-    const about_article = document.querySelectorAll('.about_article')[0];
-    const contacts_article = document.querySelectorAll('.contacts_article')[0];
+    const about_btn = document.querySelector("#about-btn");
+    const contact_btn = document.querySelector("#contact-btn");
+    const hidden_wrapper = document.querySelectorAll(".hidden-wrapper")[0];
 
     top_button.addEventListener("click", () => {
         window.scrollTo({
@@ -50,69 +62,65 @@ window.onload = () => {
             top: window.innerHeight,
             behavior: "smooth",
         });
-    })
-    
-    about_btn.addEventListener('click', () => {
-      contacts_article.classList.add('hidden')
-      about_article.classList.toggle('hidden')
-      contact_btn.classList.remove('underline')
-      about_btn.classList.toggle('underline')
-    })
+    });
 
-    contact_btn.addEventListener('click', () => {
-      about_article.classList.add('hidden')
-      contacts_article.classList.toggle('hidden')
-      contact_btn.classList.toggle('underline')
-      about_btn.classList.remove('underline')
-    })
+    about_btn.addEventListener("click", () => {
+        contacts_article.classList.add("hidden");
+        about_article.classList.toggle("hidden");
+        contact_btn.classList.remove("underline");
+        about_btn.classList.toggle("underline");
+    });
+
+    contact_btn.addEventListener("click", () => {
+        about_article.classList.add("hidden");
+        contacts_article.classList.toggle("hidden");
+        contact_btn.classList.toggle("underline");
+        about_btn.classList.remove("underline");
+    });
 
     images.forEach((el, index) => {
         el.setAttribute("data-count", `${index + 1}`);
     });
-    let data_count = document.querySelectorAll('[data-count]')
+    let data_count = document.querySelectorAll("[data-count]");
     window.onscroll = () => {
-        let img = getMostVisibleElement(data_count)
+        let img = getMostVisibleElement(data_count);
         counter1.innerText = `${img.dataset.count}/${images_counter}`;
         counter2.innerText = `${img.dataset.count}/${images_counter}`;
-    }
-    function getMostVisibleElement (els) {
+    };
+    function getMostVisibleElement(els) {
+        var viewportHeight = window.innerHeight;
 
-        var viewportHeight = window.innerHeight
-      
-        var max = 0
-        var mostVisibleEl = null
-      
+        var max = 0;
+        var mostVisibleEl = null;
+
         for (var el of Array.from(els)) {
-      
-          var rect = el.getBoundingClientRect()
-          var height = rect.bottom - rect.top
-          var visible = {
-            top: rect.top >= 0 && rect.top < viewportHeight,
-            bottom: rect.bottom > 0 && rect.bottom < viewportHeight
-          }
-      
-          var visiblePx = 0
-          if ( visible.top && visible.bottom ) {
-            visiblePx = height // Whole element is visible
-          } else if ( visible.top ) {
-            visiblePx = viewportHeight - rect.top
-          } else if ( visible.bottom ) {
-            visiblePx = rect.bottom
-          } else if ( height > viewportHeight && rect.top < 0 ) {
-            var absTop = Math.abs( rect.top )
-            if ( absTop < height ) {
-              visiblePx = height - absTop // Part of the element is visible
+            var rect = el.getBoundingClientRect();
+            var height = rect.bottom - rect.top;
+            var visible = {
+                top: rect.top >= 0 && rect.top < viewportHeight,
+                bottom: rect.bottom > 0 && rect.bottom < viewportHeight,
+            };
+
+            var visiblePx = 0;
+            if (visible.top && visible.bottom) {
+                visiblePx = height; // Whole element is visible
+            } else if (visible.top) {
+                visiblePx = viewportHeight - rect.top;
+            } else if (visible.bottom) {
+                visiblePx = rect.bottom;
+            } else if (height > viewportHeight && rect.top < 0) {
+                var absTop = Math.abs(rect.top);
+                if (absTop < height) {
+                    visiblePx = height - absTop; // Part of the element is visible
+                }
             }
-          }
-      
-          if ( visiblePx > max ) {
-            max = visiblePx
-            mostVisibleEl = el
-          }
-      
+
+            if (visiblePx > max) {
+                max = visiblePx;
+                mostVisibleEl = el;
+            }
         }
-      
-        return mostVisibleEl
-      
-      }
+
+        return mostVisibleEl;
+    }
 };
